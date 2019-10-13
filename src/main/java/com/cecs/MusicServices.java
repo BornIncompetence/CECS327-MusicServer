49cdee0dc30ed0a4bda291cf6ddd72544794fd11
Winn <winnmoo@gmail.com>
2019-10-13 15:32:39 -0700

    Added models to server

---
+++ src/main/java/com/cecs/MusicServices.java
@@ -0,0 +1,22 @@
+package com.cecs;
+
+import com.cecs.model.Music;
+import com.google.gson.GsonBuilder;
+
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class MusicServices {
+    MusicServices(){
+
+    }
+
+    public static ObservableList<Music> loadDatabase() {
+        var reader = new InputStreamReader(App.class.getResourceAsStream("/music.json"), StandardCharsets.UTF_8);
+        var musics = new GsonBuilder().create().fromJson(reader, Music[].class);
+        for (Music music : musics) {
+            music.getSong().setArtist(music.getArtist().getName());
+        }
+        return FXCollections.observableArrayList(musics);
+    }
+}
