4b8ace4062df04a0a9a3e6f22a9ea36f4f59ca37
WinnMoo <winnmoo@gmail.com>
2019-11-18 19:39:16 -0800

    Update App.java

--- src/main/java/com/cecs/App.java
+++ src/main/java/com/cecs/App.java
@@ -3,9 +3,49 @@ package com.cecs;
 import java.io.IOException;
 
 import com.cecs.DFS.DFS;
+import com.cecs.DFS.DFSCommand;
 
 public class App {
     public static void main(String[] args) {
+
+        new Thread() {
+            public void run() {
+                String[] args = new String[1];
+                args[0] = "2001";
+                try {
+                    DFSCommand.main(args);
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        new Thread(){
+            public void run() {
+                String[] args = new String[1];
+                args[0] = "2002";
+                try {
+                    DFSCommand.main(args);
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        new Thread(){
+            public void run() {
+                String[] args = new String[1];
+                args[0] = "2003";
+                try {
+                    DFSCommand.main(args);
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }.start();
 
         DFS dfs = null;
         try{
@@ -14,6 +54,8 @@ public class App {
         } catch (Exception e){
             e.printStackTrace();
         }
+
+
 
         var comm = new Communication(5500, 32768, dfs);
         try {
